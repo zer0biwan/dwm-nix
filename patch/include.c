b171561107293de6433138e0495a51ff6dd319fa
zer0biwan <59035532+zer0biwan@users.noreply.github.com>
2023-08-03 23:39:09 +0100

    Add files via upload

---
+++ patch/include.c
@@ -0,0 +1,28 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_alpha.c"
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_statusbutton.c"
+#include "bar_statuscmd.c"
+#include "bar_statuscolors.c"
+#include "bar_winicon.c"
+#include "bar_tagpreview.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+
+/* Other patches */
+#include "rotatestack.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_fibonacci.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statusbutton.h"
+#include "bar_statuscmd.h"
+#include "bar_statuscolors.h"
+#include "bar_winicon.h"
+#include "bar_tags.h"
+#include "bar_tagpreview.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "rotatestack.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
